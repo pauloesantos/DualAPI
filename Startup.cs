63339170b071b42e334aceb1bc3a2a772ebc6fe0
paulo.esantos <paulo.esantos@gmail.com>
2020-10-03 11:18:56 -0300

    Config Swagger

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace DualAPI
 {
@@ -26,6 +29,26 @@ namespace DualAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Teste DotNet DuaAPI - Squadra",
+                        Version = "v1",
+                        Description = "API REST criada com o ASP.NET Core 3.1 para Resolução do Teste da Squadra",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Paulo Eduardo Fagundes",
+                            Url = new Uri("https://github.com/pauloesantos")
+                        }
+                    });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +58,18 @@ namespace DualAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Teste DotNet Squadra");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
